cf95a068c08a24930fd49209344d88fcb514524b
Venkatesh Gopalakrishnan <96gvenkatesh@gmail.com>
2020-10-09 16:01:04 +0530

    static root added to settings

--- sastdashboard/settings.py
+++ sastdashboard/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'sast-dashboard-client', "build",
                  "static"),  # update the STATICFILES_DIRS
